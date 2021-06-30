750a92317131f7cb7992338e1303b11ed9d204e6
bishal-ghosh900 <78719302+bishal-ghosh900@users.noreply.github.com>
2021-06-30 17:52:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
+const wid = window.innerWidth;
+if(wid < 700){
+    let h = document.getElementsByClassName("htag");
+    let d = document.getElementsByClassName("proj1");
+    let iTag = document.getElementsByTagName("i");
+    for(let i = 0; i < d.length; i++){
+        d[i].removeAttribute("data-aos");
+    }
+    for(let i = 0; i < h.length; i++){
+        h[i].removeAttribute("data-aos");
+        h[i].removeAttribute("data-aos-duration");
+    }
+    for(let i = 0; i < iTag.length; i++){
+        iTag[i].removeAttribute("data-aos");
+        iTag[i].removeAttribute("data-aos-duration");
+    }
+}
+
 
